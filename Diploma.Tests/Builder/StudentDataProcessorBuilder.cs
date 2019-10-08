98e3089c1139b1ced56f4fd23d283646052a7d2b
ddumbrava <Dorina.Dumbrava@endava.com>
2019-10-08 16:01:15 +0300

    Remove ignored files, added test

---
+++ Diploma.Tests/Builder/StudentDataProcessorBuilder.cs
@@ -0,0 +1,53 @@
+﻿using Diploma.DataProcessing;
+using Diploma.Files;
+using Diploma.Utils;
+using Diploma.Validators;
+using NSubstitute;
+
+namespace Diploma.Tests.Builder
+{
+    public class StudentDataProcessorBuilder
+    {
+        private IFileReader fileReader;
+        private ICustomMapper mapper;
+        private IFileWriter fileWriter;
+        private IValidator validator;
+
+        public StudentDataProcessorBuilder()
+        {
+            this.fileReader = Substitute.For<IFileReader>();
+            this.mapper = Substitute.For<ICustomMapper>();
+            this.fileWriter = Substitute.For<IFileWriter>();
+            this.validator = Substitute.For<IValidator>();
+        }
+
+        public StudentDataProcessorBuilder WithFileReader(IFileReader fileReader)
+        {
+            this.fileReader = fileReader;
+            return this;
+        }
+
+        public StudentDataProcessorBuilder WithMapper(ICustomMapper mapper)
+        {
+            this.mapper = mapper;
+            return this;
+        }
+
+        public StudentDataProcessorBuilder WithFileWritter(IFileWriter fileWriter)
+        {
+            this.fileWriter = fileWriter;
+            return this;
+        }
+
+        public StudentDataProcessorBuilder WithValidator(IValidator validator)
+        {
+            this.validator = validator;
+            return this;
+        }
+
+        public StudentDataProcessor Build()
+        {
+            return new StudentDataProcessor(fileReader, mapper, fileWriter, validator);
+        }
+    }
+}
---
+++ Diploma.Tests/UnitTests/StudentDataProcessorTests.cs
@@ -0,0 +1,39 @@
+﻿using Diploma.Files;
+using Diploma.Models;
+using Diploma.Tests.Builder;
+using Diploma.Utils;
+using NSubstitute;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Diploma.Tests.UnitTests
+{
+    public class StudentDataProcessorTests
+    {
+        [Fact]
+        public void LoadData_CallsImportData_OneTimeWithPath()
+        {
+            string path = "path";
+            var fileReader = Substitute.For<IFileReader>();
+            var sut = new StudentDataProcessorBuilder().WithFileReader(fileReader).Build();
+
+            sut.LoadData(path);
+            fileReader.Received(1).ImportData(path);
+        }
+
+        [Fact]
+        public void LoadData_CallsMapToStudent_WithStudentsFromImportData()
+        {
+            string path = "path";
+            var students = new List<StudentRawModel>() { new StudentRawModel()};
+            var fileReader = Substitute.For<IFileReader>();
+            fileReader.ImportData(path).Returns(students);
+
+            var mapper = Substitute.For<ICustomMapper>();
+            var sut = new StudentDataProcessorBuilder().WithFileReader(fileReader).WithMapper(mapper).Build();
+
+            sut.LoadData(path);
+            mapper.Received(1).MapToStudent(students);
+        }
+    }
+}
